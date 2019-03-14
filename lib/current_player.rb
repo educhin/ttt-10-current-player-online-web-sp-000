a37049a4c6410065ddc67975a042c7f073cdee63
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-14 17:49:34 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  turns = 0
+  each.board do |turn|
+    if turn == "X" || turn == "O"
+      turns +=1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 != 0 
+    "X"
+  else
+    "O"
+  end
+end
